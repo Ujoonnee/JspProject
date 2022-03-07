1484700b1ad9fe72239a507443a81c85045b0503
Park Jin Young <pjy0302a@naver.com>
2022-03-08 04:56:18 +0900

    20220308 pjy ajax

--- JspPractice/src/main/java/co/yedam/order/OrderList.java
+++ JspPractice/src/main/java/co/yedam/order/OrderList.java
@@ -1,5 +1,8 @@
 package co.yedam.order;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,8 +15,24 @@ public class OrderList implements Command {
 		// TODO Auto-generated method stub
 		OrderService dao = new OrderServiceImpl();
 		List<OrderVO> orderList = dao.selectOrderList();
+		Map< String, Object > arrMap = new HashMap<>();
+
+		for (OrderVO orderVO : orderList) {
+			//System.out.println(orderVO.getProductName()); 
+			arrMap.put("orderNum", orderVO.getOrderNum());
+			arrMap.put("productName", orderVO.getProductName());
+			arrMap.put("userId", orderVO.getUserId());
+			arrMap.put("userName", orderVO.getUserName());
+			arrMap.put("orderDate", orderVO.getOrderDate());	
+		}
+		for (Entry<String, Object> entrySet : arrMap.entrySet()) {
+			System.out.println(entrySet.getKey() + " : " + entrySet.getValue());
+			}
 		
-		request.setAttribute("orderList", orderList);
+			System.out.println(arrMap.keySet());
+			System.out.println(arrMap.values());
+			
+		request.setAttribute("arrMap", arrMap);
 		
 		return "order/orderList.jsp";
 	}
